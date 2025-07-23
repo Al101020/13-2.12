afdec1d36f86b6b0e90b787ad65e8c134133e868
Al101020 <alex.vl.m@yandex.ru>
2025-07-23 14:56:01 +1000

    commit-02.03

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
-// const worker = new Worker('http://localhost:8080/src/worker.js'); наконецто сработал.
-const worker = new Worker('./src/worker.js');
+// const worker = new Worker('http://localhost:8080/src/worker.js');// наконецто сработал.
+// const worker = new Worker('./src/worker.js');// или это
+const worker = new Worker('../src/worker.js');
 
 const slowButton = document.querySelector('.slow-button');// console.log(slowButton);
 const consoleButton = document.querySelector('.console-button');// console.log(consoleButton);
